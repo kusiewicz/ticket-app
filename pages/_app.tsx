6be15c30d6e3931127c1b45c9fe10ad91eb8fccb
kusiewicz <kamil.kusy111@gmail.com>
2022-10-31 14:57:45 +0100

    Create ticket page and component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { ChakraProvider, ColorModeProvider, CSSReset } from '@chakra-ui/react';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <ChakraProvider>
+      <ColorModeProvider>
+        <CSSReset />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
+      </ColorModeProvider>
+    </ChakraProvider>
   );
 }
+
+export default App;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,38 @@
-import { useUser } from '@auth0/nextjs-auth0';
-import React from 'react';
+import { Button, Flex, Link, Text } from '@chakra-ui/react';
+import { NextPage } from 'next';
+import NextLink from 'next/link';
 
-const IndexPage = () => {
-  const { user, error, isLoading } = useUser();
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
-
-  console.log(user);
-
-  return <h1>pog</h1>;
+const IndexPage: NextPage = () => {
+  return (
+    <Flex direction="column" alignItems="center" padding="12rem">
+      <Flex direction="column" alignItems="center" marginBottom="6rem">
+        <Text fontSize="100px" fontWeight="800" textAlign="center">
+          The best conference ever.
+        </Text>
+        <Text fontSize="3xl" fontWeight="600">
+          Totally free too.
+        </Text>
+      </Flex>
+      <NextLink href="/api/login" passHref>
+        <Link
+          _hover={{
+            textDecoration: 'none',
+          }}
+        >
+          <Button
+            backgroundColor="purple"
+            color="white"
+            size="lg"
+            _hover={{
+              backgroundColor: 'green',
+            }}
+          >
+            Grab your ticket
+          </Button>
+        </Link>
+      </NextLink>
+    </Flex>
+  );
 };
 
 export default IndexPage;
